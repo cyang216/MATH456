0c5f06ec8efe23eee2e9304c2983a6c842a26b82
Robin Donatello <rdonatello@csuchico.edu>
2016-01-09 18:15:29 -0800

    updated structure and wk1

---
+++ stylesheets/custom.R
@@ -0,0 +1,42 @@
+cat("
+<style>
+h1 {
+  font-size: 36px;
+  font-weight: 700;
+  padding-bottom: 10px;
+  background: url('../images/bg_hr.png') repeat-x bottom;
+  color: #008000;
+}
+
+h2 {
+  font-size: 32px;
+   color:#009900;
+}
+
+h3 {
+  font-size: 24px;
+     color:#00b359;
+}
+
+
+h5 {
+  font-size: 14px;
+  font-weight: 700;
+  padding-bottom: 10px;
+  background-color: GreenYellow;
+  padding: 2px;
+  border: 1px solid DarkGreen;
+  border-radius: 3px;
+  margin: 0 5px;
+  max-width: 100%;
+  color: black;
+}
+
+body{
+  font-family: Helvetica;
+  font-size: 13pt;
+  color: black;
+}
+
+</style>
+")
