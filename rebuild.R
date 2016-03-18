c4d98b08fc3e39181931ad74f4b824ca23ebcdab
Robin Jeffries <rjeffries@csuchico.edu>
2016-03-18 14:50:47 -0700

    finish and link dimension reduction lecture

--- rebuild.R
+++ rebuild.R
@@ -8,7 +8,11 @@ render("data/Datasets.Rmd", "html_document")
 
 render("wk06.Rmd", "html_document")
 render("wk07.Rmd", "html_document")
+render("wk09.Rmd", "html_document")
 
 render("notes/lec05_Variable_Selection.Rmd", "html_document")
 render("notes/lec05_Variable_Selection.Rmd", "pdf_document")
+
+render("notes/lec06_DimensionReduction.Rmd", "html_document")
+render("notes/lec06_DimensionReduction.Rmd", "pdf_document")
 
