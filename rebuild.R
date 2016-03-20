d57ad2968c3845901bca1cc35f9ba56a01bccc21
Robin Jeffries <rjeffries@csuchico.edu>
2016-03-19 23:39:48 -0700

    split logreg away from CDA start wk10 overview for logreg update wk09 overview to add early lab finish notes up to tests of association

--- rebuild.R
+++ rebuild.R
@@ -6,13 +6,20 @@ render("HW_Info.Rmd", "html_document")
 render("data/Datasets.Rmd", "html_document")
 
 
-render("wk06.Rmd", "html_document")
-render("wk07.Rmd", "html_document")
+#render("wk06.Rmd", "html_document")
+#render("wk07.Rmd", "html_document")
 render("wk09.Rmd", "html_document")
+render("wk10.Rmd", "html_document")
 
-render("notes/lec05_Variable_Selection.Rmd", "html_document")
-render("notes/lec05_Variable_Selection.Rmd", "pdf_document")
 
-render("notes/lec06_DimensionReduction.Rmd", "html_document")
-render("notes/lec06_DimensionReduction.Rmd", "pdf_document")
+#render("notes/lec05_Variable_Selection.Rmd", "html_document")
+#render("notes/lec05_Variable_Selection.Rmd", "pdf_document")
 
+#render("notes/lec06_DimensionReduction.Rmd", "html_document")
+#render("notes/lec06_DimensionReduction.Rmd", "pdf_document")
+
+render("notes/lec07_CategoricalDataAnalysis.Rmd", "html_document")
+render("notes/lec07_CategoricalDataAnalysis.Rmd", "pdf_document")
+
+render("notes/lec08_LogisticRegression.Rmd", "html_document")
+render("notes/lec08_LogisticRegression.Rmd", "pdf_document")
